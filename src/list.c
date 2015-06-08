356ac1e1ddb1b53e88e4e06fca94f765e1d22383
Jeff Larson <thejefflarson@gmail.com>
2015-06-08 17:37:10 -0400

    list_new

--- src/list.c
+++ src/list.c
@@ -1,3 +1,5 @@
+#include "arena.h"
+
 typedef struct list_node {
   struct list_node *next;
   void *data;
@@ -6,4 +8,13 @@ typedef struct list_node {
 typedef struct list {
   list_node_t *head;
   list_node_t *tail;
+  arena_t *arena;
 } list_t;
+
+list_t *
+list_new(arena_t *arena) {
+  list_t *list = (list_t *) arena_malloc(arena, sizeof(list_t));
+  list->arena = arena;
+  return list;
+}
+
--- src/list.h
+++ src/list.h
@@ -1,6 +1,11 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include "arena.h"
+
 typedef struct list list_t;
+
+list_t *
+list_new(arena_t *arena);
 
 #endif
